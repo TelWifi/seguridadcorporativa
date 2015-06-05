b55d1e1f91f8d04a1013cbf3e671baa38985768f
Erick Orlando © <evelascom@gromero.com.pe>
2015-06-05 13:44:00 -0500

    Soporte para más de un navegador en Inicio Sesion Corporativo

--- Gromero.Corporativo.Negocio/SoloLectura/AppInfo.cs
+++ Gromero.Corporativo.Negocio/SoloLectura/AppInfo.cs
@@ -1,53 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Csla;
+﻿using Csla;
 using Csla.Data;
+using System;
 
 namespace Gromero.Corporativo.SoloLectura
 {
-    [Serializable]
-    public class AppInfo : ReadOnlyBase<AppInfo>, IEquatable<AppInfo>
-    {
-        #region Business Methods
+	[Serializable]
+	public class AppInfo : ReadOnlyBase<AppInfo>, IEquatable<AppInfo>
+	{
+		#region Business Methods
 
-        public string IdPerfilUsuario { get; set; }
-        public string NombreApp { get; set; }
-        public string Ruta { get; set; }
+		public string IdPerfilUsuario { get; set; }
+		public string NombreApp { get; set; }
+		public string Ruta { get; set; }
+		public string TipoAplicacion { get; set; }
 
-        #endregion
+		#endregion
 
-        #region Factory Methods
+		#region Factory Methods
 
-        public static AppInfo GetAppInfo(SafeDataReader data)
-        {
-            return DataPortal.Fetch<AppInfo>(data);
-        }
+		public static AppInfo GetAppInfo(SafeDataReader data)
+		{
+			return DataPortal.Fetch<AppInfo>(data);
+		}
 
-        private AppInfo()
-        { /* require use of factory methods */ }
+		private AppInfo()
+		{ /* require use of factory methods */ }
 
-        #endregion
+		#endregion
 
-        #region Data Access
+		#region Data Access
 
-        private void DataPortal_Fetch(SafeDataReader criteria)
-        {
-            IdPerfilUsuario = criteria.GetString("IdPerfilUsuario");
-            NombreApp = criteria.GetString("NombreAplicacion");
-            Ruta = criteria.GetString("Ruta");
-        }
+		private void DataPortal_Fetch(SafeDataReader criteria)
+		{
+			IdPerfilUsuario = criteria.GetString("IdPerfilUsuario");
+			NombreApp = criteria.GetString("NombreAplicacion");
+			Ruta = criteria.GetString("Ruta");
+			TipoAplicacion = criteria.GetString("TipoAplicacion");
+		}
 
-        #endregion
+		#endregion
 
-        #region Miembros de IEquatable<AppInfo>
+		#region Miembros de IEquatable<AppInfo>
 
-        public bool Equals(AppInfo other)
-        {
-            return other.NombreApp.Equals(NombreApp);
-        }
+		public bool Equals(AppInfo other)
+		{
+			return other.NombreApp.Equals(NombreApp);
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
--- Gromero.Corporativo/MainForm.cs
+++ Gromero.Corporativo/MainForm.cs
@@ -1,153 +1,154 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
-using DevExpress.XtraEditors;
-using DevExpress.XtraTreeList;
-using DevExpress.XtraTreeList.Nodes;
-using Csla;
-using Gromero.Seguridad;
-using Gromero.Corporativo.SoloLectura;
-using System.Diagnostics;
+﻿using Csla;
 using DevExpress.XtraBars.Helpers;
+using DevExpress.XtraEditors;
+using Gromero.Corporativo.SoloLectura;
+using Gromero.Seguridad;
 using Gromero.Seguridad.Windows;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace Gromero.Corporativo
 {
-    public partial class MainForm : DevExpress.XtraEditors.XtraForm
-    {
-        private string Tema = string.Empty;
+	public partial class MainForm : DevExpress.XtraEditors.XtraForm
+	{
+		private string Tema = string.Empty;
 
-        public MainForm()
-        {
-            InitializeComponent();
-            SkinHelper.InitSkinGallery(rgbSkins);
-            Program.DoLogin();
-            if (InfoUsuario.Instancia == null)
-            {
-                Application.Exit();
-                return;
-            }
-            CargaInfoUsuario();
-            Shown += (s, e) => CargarTreeView();
-            //WindowState = FormWindowState.Maximized;
-            AllowFormSkin = true;
-            rgbSkins.GalleryItemClick += (s, e) => Tema = e.Item.Caption;
-            FormClosed += (s, e) => Configuracion.Skin = Tema;
-            BarraEstado.ItemLinks.Add(lblUltimoIngreso);
-        }
+		public MainForm()
+		{
+			InitializeComponent();
+			SkinHelper.InitSkinGallery(rgbSkins);
+			Program.DoLogin();
+			if (InfoUsuario.Instancia == null)
+			{
+				Application.Exit();
+				return;
+			}
+			CargaInfoUsuario();
+			Shown += (s, e) => CargarTreeView();
+			//WindowState = FormWindowState.Maximized;
+			AllowFormSkin = true;
+			rgbSkins.GalleryItemClick += (s, e) => Tema = e.Item.Caption;
+			FormClosed += (s, e) => Configuracion.Skin = Tema;
+			BarraEstado.ItemLinks.Add(lblUltimoIngreso);
+		}
 
-        public void CargarTreeView()
-        {
-            try
-            {
-                Cursor.Current = Cursors.WaitCursor;
-                var listaApp = AppInfoList.GetAppInfoList(InfoUsuario.Instancia.IdUsuario);
-                treePerfiles.Nodes[0].Nodes.Clear();
-                foreach (var item in listaApp)
-                {
-                    //Agregamos al nodo de Aplicación
-                    var nodo = new TreeNode(item.NombreApp, 0, 0);
-                    nodo.Name = item.IdPerfilUsuario;
-                    nodo.Tag = item.Ruta;
-                    treePerfiles.Nodes[0].Nodes.Add(nodo);
-                }
-                treePerfiles.ExpandAll();
-            }
-            catch (DataPortalException ex)
-            {
-                XtraMessageBox.Show(ex.BusinessException.Message,
-                    Text,
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Exclamation);
-            }
-            catch (Exception ex)
-            {
-                XtraMessageBox.Show(ex.Message,
-                    Text,
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Exclamation);
-            }
-            finally
-            {
-                Cursor.Current = Cursors.Default;
-            }
-        }
+		public void CargarTreeView()
+		{
+			try
+			{
+				Cursor.Current = Cursors.WaitCursor;
+				var listaApp = AppInfoList.GetAppInfoList(InfoUsuario.Instancia.IdUsuario);
+				treePerfiles.Nodes[0].Nodes.Clear();
+				foreach (var item in listaApp)
+				{
+					//Agregamos al nodo de Aplicación
+					var nodo = new TreeNode(item.NombreApp, 0, 0);
+					nodo.Name = item.IdPerfilUsuario;
+					nodo.Tag = item;
+					treePerfiles.Nodes[0].Nodes.Add(nodo);
+				}
+				treePerfiles.ExpandAll();
+			}
+			catch (DataPortalException ex)
+			{
+				XtraMessageBox.Show(ex.BusinessException.Message,
+					Text,
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Exclamation);
+			}
+			catch (Exception ex)
+			{
+				XtraMessageBox.Show(ex.Message,
+					Text,
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Exclamation);
+			}
+			finally
+			{
+				Cursor.Current = Cursors.Default;
+			}
+		}
 
-        public void CargaInfoUsuario()
-        {
+		public void CargaInfoUsuario()
+		{
 
-            //if (!Csla.ApplicationContext.User.Identity.IsAuthenticated)
-            //    return;
-            lblUsuario.Caption = string.Format("Autenticado como: {0}",
-                InfoUsuario.Instancia.NombresCompletos);
-            lblCorreo.Caption = string.Format("Correo: {0}", InfoUsuario.Instancia.CorreoUsuario);
-            lblDominio.Caption = string.Format("Dominio:{0}", InfoUsuario.Instancia.Dominio);
-            lblUltimoIngreso.Caption = string.Format("Ultimo Ingreso: {0:g}",
-                InfoUsuario.Instancia.FechaUltimoIngreso);
-        }
+			//if (!Csla.ApplicationContext.User.Identity.IsAuthenticated)
+			//    return;
+			lblUsuario.Caption = string.Format("Autenticado como: {0}",
+				InfoUsuario.Instancia.NombresCompletos);
+			lblCorreo.Caption = string.Format("Correo: {0}", InfoUsuario.Instancia.CorreoUsuario);
+			lblDominio.Caption = string.Format("Dominio:{0}", InfoUsuario.Instancia.Dominio);
+			lblUltimoIngreso.Caption = string.Format("Ultimo Ingreso: {0:g}",
+				InfoUsuario.Instancia.FechaUltimoIngreso);
+		}
 
-        private void btnSalir_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
-        {
-            Close();
-        }
+		private void btnSalir_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+		{
+			Close();
+		}
 
-        private void btnLogin_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
-        {
-            Gromero.Seguridad.WinForms.LoginForm.FormAlone = false;
-            Program.DoLogin();
-            CargarTreeView();
-            CargaInfoUsuario();
-        }
+		private void btnLogin_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+		{
+			Gromero.Seguridad.WinForms.LoginForm.FormAlone = false;
+			Program.DoLogin();
+			CargarTreeView();
+			CargaInfoUsuario();
+		}
 
-        private void treePerfiles_DoubleClick(object sender, EventArgs e)
-        {
+		private void treePerfiles_DoubleClick(object sender, EventArgs e)
+		{
 
-            try
-            {
-                Cursor.Current = Cursors.WaitCursor;
-                var nodo = treePerfiles.SelectedNode;
-                if (nodo.Level == 1)
-                {
-                    var ruta = nodo.Tag as string;
-                    var urlFinal = string.Format("{0}?IdPerfilUsuario={1}", ruta, nodo.Name);
+			try
+			{
+				Cursor.Current = Cursors.WaitCursor;
+				var nodo = treePerfiles.SelectedNode;
+				if (nodo.Level == 1)
+				{
+					var app = nodo.Tag as AppInfo;
+					var urlFinal = string.Format("{0}?IdPerfilUsuario={1}", app.Ruta, nodo.Name);
 
-                    if (!ruta.EndsWith("application"))
-                        urlFinal = ruta;
+					if (!app.Ruta.EndsWith("application"))
+						urlFinal = app.Ruta;
 
-                    var process = new ProcessStartInfo("iexplore", urlFinal) { WindowStyle = ProcessWindowStyle.Hidden };
-                    Process.Start(process);
-                }
-            }
-            catch (Win32Exception)
-            {
-                XtraMessageBox.Show(
-                    "No se puede iniciar la Aplicación, asegúrese de que haya implementado. Consulte con el Administrador",
-                    Text,
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Information);
-            }
-            catch (Exception ex)
-            {
-                XtraMessageBox.Show(
-                    ex.Message,
-                    Text,
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Information);
-            }
-            finally
-            {
-                Cursor.Current = Cursors.Default;
-            }
-        }
+					ProcessStartInfo process;
 
-        private void btnActualizar_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
-        {
-            CargarTreeView();
-        }
-    }
+					if (app.TipoAplicacion == "0") // Si la App es Windows usar IE
+					{
+						process = new ProcessStartInfo("iexplore", urlFinal) { WindowStyle = ProcessWindowStyle.Hidden };
+					}
+					else
+						process = new ProcessStartInfo(urlFinal);
+
+					Process.Start(process);
+				}
+			}
+			catch (Win32Exception)
+			{
+				XtraMessageBox.Show(
+					"No se puede iniciar la Aplicación, asegúrese de que haya implementado. Consulte con el Administrador",
+					Text,
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Information);
+			}
+			catch (Exception ex)
+			{
+				XtraMessageBox.Show(
+					ex.Message,
+					Text,
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Information);
+			}
+			finally
+			{
+				Cursor.Current = Cursors.Default;
+			}
+		}
+
+		private void btnActualizar_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+		{
+			CargarTreeView();
+		}
+	}
 }
