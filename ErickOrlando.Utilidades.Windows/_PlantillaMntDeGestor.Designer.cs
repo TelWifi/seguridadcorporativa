541490050ede4e72f600c2184af35715d2a12325
Erick Orlando <evelasco@insideb2b.com>
2015-12-03 17:12:58 -0500

    PlantillaMntDeGestor terminado

--- ErickOrlando.Utilidades.Windows/_PlantillaMntDeGestor.Designer.cs
+++ ErickOrlando.Utilidades.Windows/_PlantillaMntDeGestor.Designer.cs
@@ -1,11 +1,29 @@
-﻿namespace ErickOrlando.Utilidades.Windows
+﻿using DevExpress.XtraBars;
+using ErickOrlando.Utilidades.Windows.Properties;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ErickOrlando.Utilidades.Windows
 {
     partial class _PlantillaMntDeGestor
     {
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private Bar bar1;
+        private BarDockControl barDockControlBottom;
+        private BarDockControl barDockControlLeft;
+        private BarDockControl barDockControlRight;
+        private BarDockControl barDockControlTop;
+        protected BarManager BarraBotones;
+        private Bar BarraEstado;
+        private IContainer components;
+        private BarStaticItem lblEstado;
+        protected BarButtonItem ToolActualizar;
+        protected BarButtonItem ToolCancelar;
+        protected BarButtonItem ToolGrabar;
+
 
         /// <summary>
         /// Clean up any resources being used.
@@ -29,8 +47,92 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BarraBotones = new BarManager();
+            this.bar1 = new Bar();
+            this.ToolGrabar = new BarButtonItem();
+            this.ToolCancelar = new BarButtonItem();
+            this.ToolActualizar = new BarButtonItem();
+            this.barDockControlTop = new BarDockControl();
+            this.barDockControlBottom = new BarDockControl();
+            this.barDockControlLeft = new BarDockControl();
+            this.barDockControlRight = new BarDockControl();
+            this.BarraEstado = new Bar();
+            this.lblEstado = new BarStaticItem();
+            this.BarraBotones.BeginInit();
+            base.SuspendLayout();
+            this.BarraBotones.AllowCustomization = false;
+            this.BarraBotones.AllowQuickCustomization = false;
+            this.BarraBotones.Bars.AddRange(new Bar[] { this.bar1, this.BarraEstado });
+            this.BarraBotones.DockControls.Add(this.barDockControlTop);
+            this.BarraBotones.DockControls.Add(this.barDockControlBottom);
+            this.BarraBotones.DockControls.Add(this.barDockControlLeft);
+            this.BarraBotones.DockControls.Add(this.barDockControlRight);
+            this.BarraBotones.Form = this;
+            this.BarraBotones.Items.AddRange(new BarItem[] { this.ToolGrabar, this.ToolCancelar, this.ToolActualizar, this.lblEstado });
+            this.BarraBotones.MaxItemId = 4;
+            this.BarraBotones.StatusBar = this.BarraEstado;
+            this.bar1.BarName = "Herramientas";
+            this.bar1.DockCol = 0;
+            this.bar1.DockRow = 0;
+            this.bar1.DockStyle = BarDockStyle.Top;
+            this.bar1.LinksPersistInfo.AddRange(new LinkPersistInfo[] { new LinkPersistInfo((BarItem)this.ToolGrabar), new LinkPersistInfo((BarItem)this.ToolCancelar, true), new LinkPersistInfo((BarLinkUserDefines)BarLinkUserDefines.PaintStyle, (BarItem)this.ToolActualizar, "", true, true, true, 0, null, (BarItemPaintStyle)BarItemPaintStyle.CaptionGlyph) });
+            this.bar1.Text = "Herramientas";
+            this.ToolGrabar.Caption = "&Grabar";
+            this.ToolGrabar.Glyph = Resources.db_update;
+            this.ToolGrabar.Id = 0;
+            this.ToolGrabar.Name = "ToolGrabar";
+            this.ToolGrabar.PaintStyle = BarItemPaintStyle.CaptionGlyph;
+            this.ToolCancelar.Caption = "&Cancelar";
+            this.ToolCancelar.Glyph = Resources.cerrar;
+            this.ToolCancelar.Id = 1;
+            this.ToolCancelar.Name = "ToolCancelar";
+            this.ToolCancelar.PaintStyle = BarItemPaintStyle.CaptionGlyph;
+            this.ToolActualizar.Caption = "&Actualizar";
+            this.ToolActualizar.Glyph = Resources.actualizar;
+            this.ToolActualizar.Id = 2;
+            this.ToolActualizar.Name = "ToolActualizar";
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = DockStyle.Top;
+            this.barDockControlTop.Location = new Point(0, 0);
+            this.barDockControlTop.Size = new Size(0x209, 0x1f);
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = DockStyle.Bottom;
+            this.barDockControlBottom.Location = new Point(0, 0x127);
+            this.barDockControlBottom.Size = new Size(0x209, 0x19);
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = DockStyle.Left;
+            this.barDockControlLeft.Location = new Point(0, 0x1f);
+            this.barDockControlLeft.Size = new Size(0, 0x108);
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = DockStyle.Right;
+            this.barDockControlRight.Location = new Point(0x209, 0x1f);
+            this.barDockControlRight.Size = new Size(0, 0x108);
+            this.BarraEstado.BarName = "BarraEstado";
+            this.BarraEstado.CanDockStyle = BarCanDockStyle.Bottom;
+            this.BarraEstado.DockCol = 0;
+            this.BarraEstado.DockRow = 0;
+            this.BarraEstado.DockStyle = BarDockStyle.Bottom;
+            this.BarraEstado.LinksPersistInfo.AddRange(new LinkPersistInfo[] { new LinkPersistInfo((BarItem)this.lblEstado) });
+            this.BarraEstado.OptionsBar.AllowQuickCustomization = false;
+            this.BarraEstado.OptionsBar.DrawDragBorder = false;
+            this.BarraEstado.OptionsBar.UseWholeRow = true;
+            this.lblEstado.Caption = "Listo";
+            this.lblEstado.Id = 3;
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.TextAlignment = StringAlignment.Near;
+            base.AutoScaleDimensions = new SizeF(6f, 13f);
+            base.AutoScaleMode = AutoScaleMode.Font;
+            base.ClientSize = new Size(0x209, 320);
+            base.Controls.Add(this.barDockControlLeft);
+            base.Controls.Add(this.barDockControlRight);
+            base.Controls.Add(this.barDockControlBottom);
+            base.Controls.Add(this.barDockControlTop);
+            base.EtiquetaEstado = this.lblEstado;
+            base.Name = "_PlantillaMntDeGestor";
             this.Text = "_PlantillaMntDeGestor";
+            this.BarraBotones.EndInit();
+            base.ResumeLayout(false);
+
         }
 
         #endregion
--- ErickOrlando.Utilidades.Windows/_PlantillaMntDeGestor.cs
+++ ErickOrlando.Utilidades.Windows/_PlantillaMntDeGestor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.XtraBars;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,110 @@ namespace ErickOrlando.Utilidades.Windows
 {
     public partial class _PlantillaMntDeGestor : _Plantilla
     {
+        // Methods
         public _PlantillaMntDeGestor()
         {
-            InitializeComponent();
+            ItemClickEventHandler handler = null;
+            ItemClickEventHandler handler2 = null;
+            ItemClickEventHandler handler3 = null;
+            this.components = null;
+            this.InitializeComponent();
+            this.SoloLectura = false;
+            this.Nuevo = true;
+            if (handler == null)
+            {
+                handler = (s, e) => this.Grabar();
+            }
+            this.ToolGrabar.ItemClick += handler;
+            if (handler2 == null)
+            {
+                handler2 = (s, e) => this.Cancelar();
+            }
+            this.ToolCancelar.ItemClick += handler2;
+            if (handler3 == null)
+            {
+                handler3 = (s, e) => this.Actualizar();
+            }
+            this.ToolActualizar.ItemClick += handler3;
         }
+
+        private void _Plantilla_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Cancelar();
+            }
+        }
+
+        private void _PlantillaMntDeGestor_DataGridCompleto()
+        {
+            this.Cursor = Cursors.Arrow;
+        }
+
+        public virtual void Actualizar()
+        {
+        }
+
+        public virtual void Cancelar()
+        {
+            base.DialogResult = DialogResult.Cancel;
+            base.Close();
+        }
+
+        public override void Formulario(object sender, EventArgs e)
+        {
+            base.Formulario(sender, e);
+            this.Actualizar();
+            this.ManipulaControles(base.Controls);
+            if (this.SoloLectura)
+            {
+                this.ToolGrabar.Enabled = false;
+            }
+        }
+
+        public virtual void Grabar()
+        {
+            base.DialogResult = DialogResult.OK;
+            base.Close();
+        }
+
+        public virtual void ManipulaControles(Control.ControlCollection Contenedor)
+        {
+            if (this.SoloLectura)
+            {
+                foreach (Control control in Contenedor)
+                {
+                    if ((control.Controls != null) && (control.Controls.Count > 0))
+                    {
+                        this.ManipulaControles(control.Controls);
+                    }
+                    if ((control.DataBindings.Count > 0) && (control.DataBindings[0].DataSource != null))
+                    {
+                        control.Enabled = false;
+                    }
+                }
+            }
+        }
+
+        // Properties
+        [Description("Permite editar la Barra de Herramientas"), Category("Plantilla")]
+        public bool AllowEditBarItems
+        {
+            get
+            {
+                return this.BarraBotones.AllowCustomization;
+            }
+            set
+            {
+                this.BarraBotones.AllowCustomization = value;
+            }
+        }
+
+        [Description("Permite saber si se esta creando un registro nuevo o no, por defecto es True"), Category("Plantilla")]
+        public bool Nuevo { get; set; }
+
+        [Description("Permite establecer si los controles contenidos seran de Solo Lectura"), Category("Plantilla")]
+        public bool SoloLectura { get; set; }
+
     }
 }
